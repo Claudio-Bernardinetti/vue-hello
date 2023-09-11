ea426e37be8a7d29246630420c9f9d56603c8275
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-09-11 15:05:24 +0100

    add img

--- assets/app.js
+++ assets/app.js
@@ -13,8 +13,9 @@ createApp({
         
       message: 'Hello Vue',
 
-      photo: 'https://unsplash.com/it/foto/un-edificio-molto-vecchio-con-molte-finestre-FoYzQUzePLo'
-
+      photo: 'https://images.unsplash.com/photo-1693735652425-e775076113c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      
+      description: 'Colosseum (Rome, Italy)'
       
     }
   }
